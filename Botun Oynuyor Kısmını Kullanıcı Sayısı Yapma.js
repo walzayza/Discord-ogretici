b049d87a91ae6e202f527f4221326e08f5c4bb93
recep0609 <63353458+recep0609@users.noreply.github.com>
2021-05-03 04:37:12 +0300

    Update and rename Botun-Oynuyor-Kısmını-Kullanıcı-Sayısı Yapma.js to Botun Oynuyor Kısmını Kullanıcı Sayısı Yapma.js

--- Botun Oynuyor Kısmını Kullanıcı Sayısı Yapma.js
+++ Botun Oynuyor Kısmını Kullanıcı Sayısı Yapma.js
@@ -6,6 +6,8 @@ client.on('ready', () => { //Bot hazırsa
   const guild = client.guilds.cache.get("Sunucu id'si") //Burada client.guilds.cache.get yerine client.guilds.get deneyebilirsiniz.
   //Yaptığımız işlem sunucunun id'sini çekiyor ve guild'e yazdırıyor.
   client.user.setActivity(`Toplam Üyeler: ${guild.memberCount} Kişi!`);//Burada ise botun oynuyor kısmına yazı yazıyoruz.
-  //guild.memberCount kısmındaki guild'i üstteki kısımdan atamıştık
-  //memberCount ise sunucudaki kişi sayısını belirtiyor eğer sadece memberCount yazarsanız hata verir çünkü açıklamada sunucunun id'sini çekemez
+  //guild.memberCount kısmındaki guild'i üstteki kısımdan yazdırmıştık
+  //memberCount ise sunucudaki kişi sayısını belirtiyor eğer sadece memberCount yazarsanız hata verir çünkü açıklamada oynuyor kısmında bot sunucunun id'sini çekemez.
 });
+
+client.login('Tokeniniz')//Burada hangi botun çalıştırıcağını ayarlayıbalirsiniz
