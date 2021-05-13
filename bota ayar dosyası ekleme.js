1f12d0e94a1959a22e3d5ae0ebbffb1c5d9bfe28
recep0609 <63353458+recep0609@users.noreply.github.com>
2021-05-13 10:14:03 +0300

    Create bota ayar dosyası ekleme.js

---
+++ bota ayar dosyası ekleme.js
@@ -0,0 +1,6 @@
+{
+  "prefix": "!", // botun prefixini ayarlarsınız
+  "sahip": "Kendi hesabınızın kodunu giriniz",
+  "token": "Botunuzun tokeni"
+}
+//burada kendi kodunuzu geliştirici modunu açıp herhangi bir sunucuda kendinize sağ tıklayın en altta kopyalancak id geliyor.
